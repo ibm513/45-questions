23396386fd1d22a2fc654e37978d7c9ba286105a
Ibrahim Khalid <ibmchemengg@gmail.com>
2024-07-23 17:39:54 +0500

    first commit

--- dist/index.js
+++ dist/index.js
@@ -968,13 +968,28 @@ function show_magicians() {
         console.log(magician_name[i]);
     }
 }
-console.log(show_magicians());
+show_magicians();
+//Question NO 42)Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+console.log(" ");
+console.log("Question No 42");
+console.log(" ");
+function make_great() {
+    let x_9 = magician_name.map(function (item) {
+        return (`The Great ${item}`);
+    });
+    magician_name.splice(0, magician_name.length);
+    for (let i = x_9.length - 1; i > -1; i--) {
+        magician_name.push(x_9[i]);
+    }
+}
+make_great();
+show_magicians();
+// Question No 43 Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+console.log(" ");
+console.log("Question No 43");
+console.log(" ");
 export {};
-/*42)Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
-
-43Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
-
-44)Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+/*44)Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
 
 45)Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name.
 It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs,
